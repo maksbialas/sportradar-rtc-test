f0a03320568fbedb14ea13f0bc197837cebbabeb
Maks Białas <maks.bialas@gmail.com>
2025-04-04 00:49:23 +0200

    Refactor apiHandler tests, add test for different encoding scenarios - move caching tests to a separate suite - add tests for different formatting of decoded odds and mappings

--- test/apiHandler.test.ts
+++ test/apiHandler.test.ts
@@ -11,7 +11,6 @@ const stateApiParameters = {
     ["i", "j", "k", "l", "m", "n", "o", "p"],
     ["q", "r", "s", "t", "u", "v", "w", "x"],
   ] as const,
-  nonDecodableBody: { odds: "a,b,c,d\ne,f,g,h" },
 };
 
 const mappingsApiParameters = {
@@ -24,20 +23,15 @@ const mappingsApiParameters = {
     ["id2", "value2"],
     ["idN", "valueN"],
   ]),
-  nonDecodableBody: {
-    mappings: "id1;id2:value2;id3",
-  },
 };
 
 describe.each([stateApiParameters, mappingsApiParameters])(
   "$ApiHandler.name API handler",
-  ({ ApiHandler, responseBody, expectedData, nonDecodableBody }) => {
+  ({ ApiHandler, responseBody, expectedData }) => {
     beforeEach(() => {
       // mock fetch to return responseBody in a response body
       vi.spyOn(global, "fetch").mockResolvedValue(
-        new Response(JSON.stringify(responseBody), {
-          headers: { ETag: `W/"123"` },
-        }),
+        new Response(JSON.stringify(responseBody)),
       );
     });
 
@@ -66,13 +60,24 @@ describe.each([stateApiParameters, mappingsApiParameters])(
       const apiHandler = new ApiHandler();
       await expect(() => apiHandler.getData()).rejects.toThrowError();
     });
+  },
+);
 
-    it("should throw errror if response data is not decodable", async () => {
-      vi.spyOn(global, "fetch").mockResolvedValue(
-        new Response(JSON.stringify(nonDecodableBody)),
+describe.each([stateApiParameters, mappingsApiParameters])(
+  "Caching: $ApiHandler.name API handler",
+  ({ ApiHandler, responseBody }) => {
+    beforeEach(() => {
+      // mock fetch to return responseBody in a response body
+      vi.spyOn(global, "fetch").mockImplementation(
+        async () =>
+          new Response(JSON.stringify(responseBody), {
+            headers: { ETag: `W/"123"` },
+          }),
       );
-      const apiHandler = new ApiHandler();
-      await expect(() => apiHandler.getData()).rejects.toThrowError();
+    });
+
+    afterEach(() => {
+      vi.restoreAllMocks();
     });
 
     it("should call API url with HEAD request for caching", async () => {
@@ -96,16 +101,11 @@ describe.each([stateApiParameters, mappingsApiParameters])(
       });
 
       // 2nd fetching
-      vi.spyOn(global, "fetch").mockResolvedValue(
-        new Response(JSON.stringify(responseBody), {
-          headers: { ETag: `W/"123"` },
-        }),
-      );
       await apiHandler.getData();
-      expect(global.fetch).toHaveBeenNthCalledWith(1, apiHandler.apiUrl, {
+      expect(global.fetch).toHaveBeenNthCalledWith(3, apiHandler.apiUrl, {
         method: "HEAD",
       });
-      expect(global.fetch).not.toHaveBeenNthCalledWith(2, apiHandler.apiUrl, {
+      expect(global.fetch).not.toHaveBeenNthCalledWith(4, apiHandler.apiUrl, {
         method: "GET",
       });
     });
@@ -123,18 +123,109 @@ describe.each([stateApiParameters, mappingsApiParameters])(
       });
 
       // 2nd fetching
-      vi.spyOn(global, "fetch").mockResolvedValue(
+      const nonCachedFetch = vi.spyOn(global, "fetch").mockResolvedValue(
         new Response(JSON.stringify(responseBody), {
           headers: { ETag: `W/"456"` },
         }),
       );
       await apiHandler.getData();
-      expect(global.fetch).toHaveBeenNthCalledWith(1, apiHandler.apiUrl, {
+      expect(nonCachedFetch).toHaveBeenNthCalledWith(1, apiHandler.apiUrl, {
         method: "HEAD",
       });
-      expect(global.fetch).toHaveBeenNthCalledWith(2, apiHandler.apiUrl, {
+      expect(nonCachedFetch).toHaveBeenNthCalledWith(2, apiHandler.apiUrl, {
         method: "GET",
       });
     });
   },
 );
+
+describe("Encoded odds", () => {
+  function getDataPrepared(odds: string) {
+    const stateHandler = new StateApiHandler();
+    vi.spyOn(global, "fetch").mockResolvedValue(
+      new Response(JSON.stringify({ odds })),
+    );
+    return stateHandler.getData();
+  }
+
+  it("should be extracted from an empty string", async () => {
+    expect(await getDataPrepared("")).toEqual([]);
+  });
+
+  it("should be extracted from single line", async () => {
+    const expectedData = [["1", "2", "3", "4", "5", "6", "7", "8"]];
+
+    expect(await getDataPrepared("1,2,3,4,5,6,7,8")).toEqual(expectedData);
+    expect(await getDataPrepared("1,2,3,4,5,6,7,8,")).toEqual(expectedData);
+    expect(await getDataPrepared("1,2,3,4,5,6,7,8,\n")).toEqual(expectedData);
+    expect(await getDataPrepared("1,2,3,4,5,6,7,8\n")).toEqual(expectedData);
+  });
+
+  it("should be extracted when no score", async () => {
+    expect(await getDataPrepared("1,2,3,4,5,6,7,")).toEqual([
+      ["1", "2", "3", "4", "5", "6", "7", null],
+    ]);
+  });
+
+  it("should raise an error on a line with entries <7 or >8", async () => {
+    await expect(
+      async () => await getDataPrepared("1,2,3,4,5,6,7,8,9"),
+    ).rejects.toThrowError();
+    await expect(
+      async () => await getDataPrepared("1,2,3,4,5,6"),
+    ).rejects.toThrowError();
+  });
+
+  it("should extract multiline data", async () => {
+    expect(
+      await getDataPrepared(
+        "1,2,3,4,5,6,7,\n1,2,3,4,5,6,7,8,\n1,2,3,4,5,6,7,8",
+      ),
+    ).toEqual([
+      ["1", "2", "3", "4", "5", "6", "7", null],
+      ["1", "2", "3", "4", "5", "6", "7", "8"],
+      ["1", "2", "3", "4", "5", "6", "7", "8"],
+    ]);
+  });
+});
+
+describe("Encoded mappings", () => {
+  function getDataPrepared(mappings: string) {
+    const mappingsHandler = new MappingsApiHandler();
+    vi.spyOn(global, "fetch").mockResolvedValue(
+      new Response(JSON.stringify({ mappings })),
+    );
+    return mappingsHandler.getData();
+  }
+
+  it("should be extracted from an empty string", async () => {
+    expect(await getDataPrepared("")).toEqual(new Map());
+  });
+
+  it("should be extracted from a line", async () => {
+    const expectedData = new Map([
+      ["key1", "val1"],
+      ["key2", "val2"],
+      ["key3", "val3"],
+    ]);
+
+    expect(await getDataPrepared("key1:val1;key2:val2;key3:val3")).toEqual(
+      expectedData,
+    );
+    expect(await getDataPrepared("key1:val1;key2:val2;key3:val3;")).toEqual(
+      expectedData,
+    );
+  });
+
+  it("should raise an error on no value", async () => {
+    await expect(
+      async () => await getDataPrepared("key1:val1;key2;key3:val3"),
+    ).rejects.toThrowError();
+  });
+
+  it("should raise an error on no key or value", async () => {
+    await expect(
+      async () => await getDataPrepared("key1:val1;;key3:val3"),
+    ).rejects.toThrowError();
+  });
+});
