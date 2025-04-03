2519cc2f472275ae7940666e5c4b071a1c93c4b6
Maks Białas <maks.bialas@gmail.com>
2025-04-03 12:06:53 +0200

    Fulfill error apiHandler tests - replace anys with unknowns - validate response shape - check decoded shape

--- src/apiHandlers.ts
+++ src/apiHandlers.ts
@@ -14,12 +14,29 @@ export class StateApiHandler {
   apiUrl = "http://localhost:3000/api/state";
   async getData(): Promise<Tuple8[]> {
     const response = await fetch(this.apiUrl);
-    const data = await response.json();
-    const oddsEncoded: string = data.odds;
+    const data: unknown = await response.json();
+
+    if (
+      typeof data !== "object" ||
+      data === null ||
+      !("odds" in data) ||
+      typeof data.odds !== "string"
+    )
+      throw new TypeError("'odds' string not found in response");
+
+    const oddsEncoded = data.odds;
 
     const oddsUnmapped = oddsEncoded
-      .split(",\n")
+      .replaceAll(/,(\n|$)/g, "$1")
+      .split("\n", -1)
       .map((sportEvent) => sportEvent.split(","));
+
+    oddsUnmapped.forEach((sportEvent, idx) => {
+      if (sportEvent.length !== 8)
+        throw new TypeError(
+          `'odds' contain elements with length different than 8 (row ${idx}: ${JSON.stringify(sportEvent)})`,
+        );
+    });
 
     return oddsUnmapped as Tuple8[];
   }
@@ -31,13 +48,28 @@ export class MappingsApiHandler {
   apiUrl = "http://localhost:3000/api/mappings";
   async getData(): Promise<Map<string, string>> {
     const response = await fetch(this.apiUrl);
-    const data = await response.json();
-    const mappingsEncoded: string = data.mappings;
+    const data: unknown = await response.json();
+
+    if (
+      typeof data !== "object" ||
+      data === null ||
+      !("mappings" in data) ||
+      typeof data.mappings !== "string"
+    )
+      throw new TypeError("'mappings' string not found in response");
+
+    const mappingsEncoded = data.mappings;
 
     const mappingsSplit = mappingsEncoded
       .split(";")
       .map((mapping) => mapping.split(":"));
 
+    mappingsSplit.forEach((mapping, idx) => {
+      if (mapping.length !== 2)
+        throw new TypeError(
+          `'mappings' contain elements with length different than 2 (row ${idx})`,
+        );
+    });
     const mappingsTuples: [string, string][] = mappingsSplit as [
       string,
       string,
