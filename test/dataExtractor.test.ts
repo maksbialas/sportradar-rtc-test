1240ecf3df6864486da619cb3d413334ab4e0c33
Maks Białas <maks.bialas@gmail.com>
2025-04-04 01:45:39 +0200

    Change data extractor tests to handle unresolved mappings according to requirements - when mapping is unresolved, skip processing for that record only instead of aborting on whole request

--- test/dataExtractor.test.ts
+++ test/dataExtractor.test.ts
@@ -5,6 +5,22 @@ import { MappingsApiHandler, StateApiHandler } from "../src/apiHandlers";
 const mockStateHandler: StateApiHandler = new StateApiHandler();
 const mockMappingsHandler: MappingsApiHandler = new MappingsApiHandler();
 
+const extractedOutput: SportEvent[] = [
+  {
+    id: "a",
+    sport: "FOOTBALL",
+    competition: "La Liga",
+    startTimeTs: 1234,
+    homeCompetitor: "FC Barcelona",
+    awayCompetitor: "Real Madrid",
+    status: "PRE",
+    scores: new Map([
+      ["CURRENT", { home: 5, away: 1 }],
+      ["PERIOD_1", { home: 3, away: 0 }],
+    ]),
+  },
+];
+
 describe("Sport Event data extractor", () => {
   beforeEach(() => {
     vi.spyOn(mockMappingsHandler, "getData").mockResolvedValue(
@@ -34,39 +50,27 @@ describe("Sport Event data extractor", () => {
       mockMappingsHandler,
     );
 
-    const desiredOutput: SportEvent[] = [
-      {
-        id: "a",
-        sport: "FOOTBALL",
-        competition: "La Liga",
-        startTimeTs: 1234,
-        homeCompetitor: "FC Barcelona",
-        awayCompetitor: "Real Madrid",
-        status: "PRE",
-        scores: new Map([
-          ["CURRENT", { home: 5, away: 1 }],
-          ["PERIOD_1", { home: 3, away: 0 }],
-        ]),
-      },
-    ];
-
-    expect(await extractor.extract()).toEqual(desiredOutput);
+    expect(await extractor.extract()).toEqual(extractedOutput);
   });
 
-  it("should throw an error when a key is not present in mappings", async () => {
-    // mappings are empty
-    vi.spyOn(mockMappingsHandler, "getData").mockResolvedValue(new Map([]));
+  it("should skip processing when a key is not present in mappings", async () => {
+    // odds have key not present in mappings
+    vi.spyOn(mockStateHandler, "getData").mockResolvedValue([
+      ["a", "b", "c", "1234", "e", "f", "g", "h@5:1|i@3:0"],
+      ["a", "b", "c", "1234", "x", "f", "g", "h@5:1|i@3:0"],
+    ]);
+    const errorSpy = vi.spyOn(console, "error");
 
     const extractor = new SportEventDataExtractor(
       mockStateHandler,
       mockMappingsHandler,
     );
 
-    await expect(() => extractor.extract()).rejects.toThrowError();
+    expect(await extractor.extract()).toEqual(extractedOutput);
+    expect(errorSpy).toBeCalled();
   });
 
   it("should throw an error when score encoding is malformed", async () => {
-    // mappings are empty
     vi.spyOn(mockStateHandler, "getData").mockResolvedValue([
       ["a", "b", "c", "1234", "e", "f", "g", "h@1||3:0"],
     ]);
@@ -75,7 +79,6 @@ describe("Sport Event data extractor", () => {
       mockStateHandler,
       mockMappingsHandler,
     );
-
     await expect(() => extractor.extract()).rejects.toThrowError();
   });
 
